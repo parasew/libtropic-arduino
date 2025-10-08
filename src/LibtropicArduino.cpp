9a0e29c113019477d03205b15889e828360b4f6f
Jan Medek <czmedexs@gmail.com>
2025-10-08 15:10:53 +0200

    feat: create class Tropic01, implement begin() and end() methods

---
+++ src/LibtropicArduino.cpp
@@ -0,0 +1,25 @@
+#include "LibtropicArduino.h"
+
+lt_ret_t Tropic01::begin(uint16_t spi_cs_pin, unsigned int rng_seed
+#ifdef LT_USE_INT_PIN
+                         ,
+                         uint16_t int_gpio_pin
+#endif
+                         ,
+                         SPIClass &spi, SPISettings spi_settings)
+{
+    // Initialize device structure
+    this->device.spi_cs_pin = spi_cs_pin;
+#ifdef LT_USE_INT_PIN
+    this->device.int_gpio_pin = int_gpio_pin;
+#endif
+    this->device.spi_settings = spi_settings;
+    this->device.rng_seed = rng_seed;
+    this->device.spi = &spi;
+    // Initialize handle
+    this->handle.l2.device = &this->device;
+
+    return lt_init(&this->handle);
+}
+
+lt_ret_t Tropic01::end(void) { return lt_deinit(&this->handle); }
---
+++ src/LibtropicArduino.h
@@ -0,0 +1,28 @@
+#ifndef LIBTROPIC_ARDUINO_H
+#define LIBTROPIC_ARDUINO_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <stdarg.h>
+
+#include "libtropic.h"
+#include "libtropic_common.h"
+#include "libtropic_port_arduino.h"
+
+class Tropic01 {
+   public:
+    lt_ret_t begin(uint16_t spi_cs_pin, unsigned int rng_seed
+#ifdef LT_USE_INT_PIN
+                   ,
+                   uint16_t int_gpio_pin
+#endif
+                   ,
+                   SPIClass &spi = ::SPI, SPISettings spi_settings = SPISettings(10000000, MSBFIRST, SPI_MODE0));
+    lt_ret_t end(void);
+
+   private:
+    lt_dev_arduino_t device;
+    lt_handle_t handle;
+};
+
+#endif  // LIBTROPIC_ARDUINO_H

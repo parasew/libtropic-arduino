1e8f4293ea47411dd96b5d0da21ee53573ebd5c4
Jan Medek <czmedexs@gmail.com>
2025-11-10 18:57:52 +0100

    feat(class Tropic01): implement secure session establishing and ping command

--- src/LibtropicArduino.cpp
+++ src/LibtropicArduino.cpp
@@ -1,12 +1,14 @@
 #include "LibtropicArduino.h"
 
-lt_ret_t Tropic01::begin(uint16_t spi_cs_pin, unsigned int rng_seed
+Tropic01::~Tropic01(void) { this->end(); }
+
+lt_ret_t Tropic01::begin(const uint16_t spi_cs_pin, const unsigned int rng_seed
 #ifdef LT_USE_INT_PIN
                          ,
-                         uint16_t int_gpio_pin
+                         const uint16_t int_gpio_pin
 #endif
                          ,
-                         SPIClass &spi, SPISettings spi_settings)
+                         SPIClass& spi, SPISettings spi_settings)
 {
     // Initialize device structure
     this->device.spi_cs_pin = spi_cs_pin;
@@ -16,12 +18,40 @@ lt_ret_t Tropic01::begin(uint16_t spi_cs_pin, unsigned int rng_seed
     this->device.spi_settings = spi_settings;
     this->device.rng_seed = rng_seed;
     this->device.spi = &spi;
-    // Initialize handle
+    // Pass device structure to handle
     this->handle.l2.device = &this->device;
+
+    // Initialize crypto context structure and pass to handle
+    this->handle.l3.crypto_ctx = &this->crypto_ctx;
 
     return lt_init(&this->handle);
 }
 
-lt_ret_t Tropic01::end(void) { return lt_deinit(&this->handle); }
+lt_ret_t Tropic01::end(void)
+{
+    lt_ret_t ret_abort = LT_OK, ret_deinit = LT_OK;
 
-Tropic01::~Tropic01(void) { this->end(); }
+    if (this->handle.l3.session_status == LT_SECURE_SESSION_ON) {
+        ret_abort = this->secureSessionEnd();
+    }
+
+    ret_deinit = lt_deinit(&this->handle);
+
+    if (ret_abort != LT_OK) {
+        return ret_abort;
+    }
+
+    return ret_deinit;
+}
+
+lt_ret_t Tropic01::secureSessionStart(const uint8_t* shipriv, const uint8_t* shipub, const lt_pkey_index_t pkey_index)
+{
+    return lt_verify_chip_and_start_secure_session(&this->handle, shipriv, shipub, pkey_index);
+}
+
+lt_ret_t Tropic01::secureSessionEnd(void) { return lt_session_abort(&this->handle); }
+
+lt_ret_t Tropic01::ping(const uint8_t* msg_out, uint8_t* msg_in, const uint16_t msg_len)
+{
+    return lt_ping(&this->handle, msg_out, msg_in, msg_len);
+}
--- src/LibtropicArduino.h
+++ src/LibtropicArduino.h
@@ -3,26 +3,31 @@
 
 #include <Arduino.h>
 #include <SPI.h>
-#include <stdarg.h>
 
 #include "libtropic.h"
 #include "libtropic_common.h"
+#include "libtropic_mbedtls_v4.h"
 #include "libtropic_port_arduino.h"
 
 class Tropic01 {
    public:
     ~Tropic01(void);
-    lt_ret_t begin(uint16_t spi_cs_pin, unsigned int rng_seed
+    lt_ret_t begin(const uint16_t spi_cs_pin
 #ifdef LT_USE_INT_PIN
                    ,
-                   uint16_t int_gpio_pin
+                   const uint16_t int_gpio_pin
 #endif
                    ,
-                   SPIClass &spi = ::SPI, SPISettings spi_settings = SPISettings(10000000, MSBFIRST, SPI_MODE0));
+                   const unsigned int rng_seed = random(), SPIClass& spi = ::SPI,
+                   SPISettings spi_settings = SPISettings(10000000, MSBFIRST, SPI_MODE0));
     lt_ret_t end(void);
+    lt_ret_t secureSessionStart(const uint8_t* shipriv, const uint8_t* shipub, const lt_pkey_index_t pkey_index);
+    lt_ret_t secureSessionEnd(void);
+    lt_ret_t ping(const uint8_t* msg_out, uint8_t* msg_in, const uint16_t msg_len);
 
    private:
     lt_dev_arduino_t device;
+    lt_ctx_mbedtls_v4_t crypto_ctx;
     lt_handle_t handle;
 };
 
